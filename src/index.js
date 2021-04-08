94d55322bc42aa8e3bead92c3b17649cc4d0ed77
AndyWang <wangyuanwenqi@outlook.com>
2021-04-08 18:04:19 +0800

    update umd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './lib/wx-debug';
 
 import { connect, MqttClient, Store } from './lib';
-export { connect, MqttClient, Store };
-export default { connect, MqttClient, Store };
+// export { connect, MqttClient, Store };
+
+const mqtt = { connect, MqttClient, Store };
+export default mqtt;
